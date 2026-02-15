2cb48b263f7fdd7383edc763fa2014f9f61c632d
Melat Tewachew <melattewachew@gmail.com>
2026-02-15 18:44:50 +0300

    folder setup

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from src.config import PROJECT_ROOT, RAW_DATA_PATH, PRODUCT_MAPPING, TARGET_PRODUCTS, DTYPE_STRATEGY, CHUNK_SIZE, MIN_WORDS
+from src.data_loader import load_complaints_data, stream_complaints, filter_complaints_streaming
+from src.preprocessor import fast_filter_pipeline, prepare_final_dataset
+from src.visualizer import create_product_dashboard, create_text_length_plot, create_missing_data_plot, create_stratified_sample_plot, create_data_quality_dashboard
+from src.reporter import save_data_quality_report, generate_task1_summary
